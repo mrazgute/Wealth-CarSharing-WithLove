2d01653e0157cb2a48fa066a0dfc2ea59c2e39d2
Jonas Antanaitis <joant@danskebank.lt>
2018-09-06 13:52:24 +0300

    styles

---
+++ src/screens/DriverDetails.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class DriverDetails extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return <React.Fragment>
+            <div className="tbg">
+                <div className="theader">
+                    <div className="tlogo">
+                        <img src="https://worldvectorlogo.com/logos/tinder-1.svg" alt="Tinder Logo" title="Tinder Logo" />
+                    </div>
+                </div>
+                <div className="tbgwrap">
+                </div>
+            </div>
+        </React.Fragment>;
+    }
+}
+
+export default DriverDetails;
