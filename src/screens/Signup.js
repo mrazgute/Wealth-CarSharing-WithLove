6aa1d1e6356f1caf1e5b1e4d924dbff50ac079c7
gediminasmorkys <gediminas.morkys@gmail.com>
2018-09-06 13:46:12 +0300

    Signup screen fix

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import 'login.css';
+import '../css/login.css';
 
 class Signup extends Component {
   constructor() {
