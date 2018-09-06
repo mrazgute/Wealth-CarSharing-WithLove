4dd79cb489d193da2d6c6fd02b64857a058d2152
Jonas Antanaitis <joant@danskebank.lt>
2018-09-06 15:48:22 +0300

    css updated

--- src/App.js
+++ src/App.js
@@ -10,28 +10,28 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div className="tbg">
-      <div className="theader">
-        <i className="fa fa-cog" aria-hidden="true"></i>
-        <i className="fa fa-comments" aria-hidden="true"></i>
-        <div className="tlogo">
-          <img src="https://worldvectorlogo.com/logos/tinder-1.svg" alt="Tinder Logo" title="Tinder Logo" />
-        </div>
-      </div>
-        <Switch>
-          {/* Users sign up here*/}
-          <Route exact path="/signup" component={Signup}/>
-          {/* Users select roles here*/}
-          <Route exact path="/role-selection" component={RoleSelection}/>
-          {/* driver enters details and waits here (also gets match notification here): */}
-          <Route exact path="/driver-details" component={DriverDetails}/>
-          {/* passenger enters details and waits here (also gets match notification here): */}
-          <Route exact path="/passenger-details" render={() => <div>passenger enters details and waits here (also gets match notification here)</div>}/>
+        <div className="tbg">
+          <div className="theader">
+            <i className="fa fa-cog" aria-hidden="true"></i>
+            <i className="fa fa-align-justify" aria-hidden="true"></i>
+            <div className="tlogo">
+              <img src="../assets/logo.PNG" alt="Tinder Logo" title="Tinder Logo" />
+            </div>
+          </div>
+          <Switch>
+            {/* Users sign up here*/}
+            <Route exact path="/signup" component={Signup} />
+            {/* Users select roles here*/}
+            <Route exact path="/role-selection" component={RoleSelection} />
+            {/* driver enters details and waits here (also gets match notification here): */}
+            <Route exact path="/driver-details" component={DriverDetails} />
+            {/* passenger enters details and waits here (also gets match notification here): */}
+            <Route exact path="/passenger-details" render={() => <div>passenger enters details and waits here (also gets match notification here)</div>} />
 
-          <Route exact path="/matching-screen" render={() => <div>matching screen</div>}/>
-          {/* list of trips matched: */}
-          <Route exact path="/trips" render={() => <div>list of trips matched</div>}/>
-        </Switch>
+            <Route exact path="/matching-screen" render={() => <div>matching screen</div>} />
+            {/* list of trips matched: */}
+            <Route exact path="/trips" render={() => <div>list of trips matched</div>} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
